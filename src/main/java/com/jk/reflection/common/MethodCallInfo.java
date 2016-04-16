fe2ca8fe03729647f04dc351f54b5c3c68fd797b
Jalal Kiswani <kiswanij@yahoo.com>
2016-04-16 05:43:30 +0300

    First version of Renmote Reflection

---
+++ src/main/java/com/jk/reflection/common/MethodCallInfo.java
@@ -0,0 +1,172 @@
+package com.jk.reflection.common;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class MethodCallInfo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String className;
+	private String methodName;
+	private Object[] paramters;
+	private boolean failed;
+	private Exception exception;
+	private Object result;
+
+	/**
+	 * 
+	 */
+	public MethodCallInfo() {
+	}
+
+	/**
+	 * 
+	 * @param className
+	 * @param methodName
+	 * @param paramters
+	 */
+	public MethodCallInfo(String className, String methodName, Object... paramters) {
+		this.className = className;
+		this.methodName = methodName;
+		this.paramters = paramters;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getClassName() {
+		return className;
+	}
+
+	/**
+	 * 
+	 * @param className
+	 */
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getMethodName() {
+		return methodName;
+	}
+
+	/**
+	 * 
+	 * @param methodName
+	 */
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Object[] getParamters() {
+		return paramters;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	public Class[] getParamtersTypes() {
+		Class[] types = new Class[paramters.length];
+		for (int i = 0; i < types.length; i++) {
+			types[i] = paramters[i].getClass();
+		}
+		return types;
+	}
+
+	/**
+	 * 
+	 * @param param
+	 */
+	public void setParamters(Object... param) {
+		this.paramters = param;
+	}
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public void setException(Exception e) {
+		this.exception = e;
+		failed = true;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isFailed() {
+		return failed;
+	}
+
+	/**
+	 * 
+	 * @param failed
+	 */
+	public void setFailed(boolean failed) {
+		this.failed = failed;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Exception getException() {
+		return exception;
+	}
+
+	/**
+	 * 
+	 * @param result
+	 */
+	public void setResult(Object result) {
+		this.result = result;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Object getResult() {
+		if (isFailed()) {
+			throw new IllegalStateException("Cannot call getResult on failed method call , "
+					+ "please check isFailed() , and if failed , please call getException() for more info\n"
+					+ exception.getMessage());
+		}
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param another
+	 */
+	public void set(MethodCallInfo another) {
+		this.failed = another.failed;
+		this.exception = another.exception;
+		this.result = another.result;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer b = new StringBuffer();
+		b.append("[Class :".concat(getClassName().concat(", ")));
+		b.append("Method :".concat(getMethodName().concat(", ")));
+		b.append("Paramters :".concat(Arrays.toString(getParamters()).concat(", ")));
+		b.append("Types :".concat(Arrays.toString(getParamtersTypes()) + ", "));
+		b.append("Result :".concat(getResult() + ", "));
+		b.append("Exception :".concat(getException() + "]"));
+
+		return b.toString();
+	}
+
+}
